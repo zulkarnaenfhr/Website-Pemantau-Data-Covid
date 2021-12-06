5440c523460b01f11fb05cc48bb1c75e3a5d6488
Fahri Izuddin Zulkarnaen <fahrizulkarnaen1@gmail.com>
2021-12-06 13:37:08 +0700

    final

--- data tiap provinsi.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $APIVaksin = "https://data.covid19.go.id/public/api/prov_list.json";
-    $konten = file_get_contents($APIVaksin);
-    $data = json_decode($konten, true);
-    $dataVaksin = $data['list_data'];
-    
-    $jakarta = $dataVaksin[0]['key'];
-    echo $jakarta;
-?>
